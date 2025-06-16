bf99f5699c663564ac11afac72d7c473e0d12d24
Nastya <trukhina12345@mail.ru>
2025-06-16 21:11:16 +0300

    Добавилось оформление item_task

--- app/src/main/java/ru/anasttruh/taskhelper/MainActivity.kt
+++ app/src/main/java/ru/anasttruh/taskhelper/MainActivity.kt
@@ -2,14 +2,15 @@ package ru.anasttruh.taskhelper
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import ru.anasttruh.taskhelper.AddTaskActivity
 import ru.anasttruh.taskhelper.databinding.ActivityMainBinding
+import ru.anasttruh.taskhelper.Task
+import ru.anasttruh.taskhelper.TaskAdapter
+import ru.anasttruh.taskhelper.StatsActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,16 +18,25 @@ class MainActivity : AppCompatActivity() {
     private val taskList = mutableListOf<Task>()
     private lateinit var adapter: TaskAdapter
 
-    companion object {
-        private const val ADD_TASK_REQUEST_CODE = 1
+    private val addTaskLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val task = result.data?.getSerializableExtra("task") as? Task
+            task?.let {
+                taskList.add(it)
+                adapter.notifyDataSetChanged()
+                Log.d("TaskHelper", "Добавлена задача: ${it.title}")
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root) // <-- исправлено
+        setContentView(binding.root)
 
-        binding.taskList.layoutManager = LinearLayoutManager(this) // <-- добавлено
+        binding.taskList.layoutManager = LinearLayoutManager(this)
 
         adapter = TaskAdapter(taskList) { task ->
             Log.d("TaskHelper", "Click on task: ${task.title}")
@@ -36,26 +46,14 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnAddTask.setOnClickListener {
             val intent = Intent(this, AddTaskActivity::class.java)
-            startActivityForResult(intent, ADD_TASK_REQUEST_CODE)
+            addTaskLauncher.launch(intent)
         }
 
         binding.btnStats.setOnClickListener {
             val intent = Intent(this, StatsActivity::class.java)
             startActivity(intent)
         }
-    }
 
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if(requestCode == ADD_TASK_REQUEST_CODE && resultCode == RESULT_OK && data != null){
-            val task = data.getSerializableExtra("task") as? Task
-            task?.let{
-                taskList.add(it)
-                adapter.notifyDataSetChanged()
-                Log.d("TaskHelper", "Добавлена задача: ${it.title}")
-            }
-        }
     }
 }
--- app/src/main/java/ru/anasttruh/taskhelper/TaskAdapter.kt
+++ app/src/main/java/ru/anasttruh/taskhelper/TaskAdapter.kt
@@ -5,6 +5,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class TaskAdapter(
     private val tasks: MutableList<Task>,
@@ -28,13 +31,29 @@ class TaskAdapter(
     class TaskViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         private val titleTextView: TextView = itemView.findViewById(R.id.textTitle)
         private val descTextView: TextView = itemView.findViewById(R.id.textDescription)
+        private val deadlineTextView: TextView = itemView.findViewById(R.id.textDeadline)
 
         fun bind(task: Task, onItemClick: (Task) -> Unit){
             titleTextView.text = task.title
             descTextView.text = task.fullDesc
+            if (task.deadline == null || task.deadline == "null") {
+                deadlineTextView.text = ""
+            } else {
+                deadlineTextView.text = secToDate(task.deadline)
+            }
             itemView.setOnClickListener {
                 onItemClick(task)
             }
         }
+
+        fun secToDate(timestampStr: String): String {
+            val timestamp = timestampStr.toLong()
+            val date = Date(timestamp)
+
+            val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            return "Срок задачи ${sdf.format(date)}"
+        }
     }
+
+
 }
